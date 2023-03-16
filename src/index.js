0c7521e4d83eb59166b3687c0e25abdba4acd6d4
Do Ba Toan <58694203+AMVND@users.noreply.github.com>
2023-03-16 14:51:38 +0700

    toan_update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 import reportWebVitals from './reportWebVitals'
 
 import { createStore } from 'redux'
@@ -21,13 +21,14 @@ const store = createStore(
 
 document.title = 'Tua CRM'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <Provider store={store}>
     <React.StrictMode>
       <Layout />
     </React.StrictMode>
   </Provider>,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
